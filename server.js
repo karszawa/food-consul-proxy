8719abf5dbad33fb9688300c33c0d78a555cd834
Hiroaki KARASAWA <siqvare@gmail.com>
2018-11-09 19:15:09 +0900

    Rename app.js to server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || 8080; 
+const port = process.env.PORT || 8080;
 
 const proxy = require('cors-anywhere');
 
-proxy.createServer({
+proxy
+  .createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
+    removeHeaders: ['cookie', 'cookie2'],
+  })
+  .listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
+  });
