d6eccd80fe96b807b6e4061431a03de34b77a68b
Hiroaki KARASAWA <siqvare@gmail.com>
2018-11-12 13:50:25 +0900

    :lipstick:

--- server.go
+++ server.go
@@ -22,6 +22,14 @@ func main() {
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if r.Method == http.MethodOptions {
 				w.Header().Set("Access-Control-Allow-Origin", "*")
+				w.Header().Set(
+					"Access-Control-Allow-Headers",
+					strings.Join(r.Header["Access-Control-Request-Headers"], ","),
+				)
+				w.Header().Set(
+					"Access-Control-Allow-Method",
+					strings.Join(r.Header["Access-Control-Request-Method"], ","),
+				)
 				w.WriteHeader(http.StatusOK)
 
 				return
